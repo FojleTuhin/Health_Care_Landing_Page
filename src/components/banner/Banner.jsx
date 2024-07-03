7b37b09b798cc1d50e1995ffbf82a9bbfe37d7b0
fojletuhin <fojletuhin@gmail.com>
2024-07-03 22:53:03 +0600

    add linear gradient on banner img

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,8 +1,14 @@
 
 const Banner = () => {
     return (
-        <div>
+    
+        <div className="relative">
             <img className="rounded-2xl md:rounded-[48px]" src="https://i.ibb.co/XxKgHFQ/Rectangle-5.png" alt="" />
+            <div className="absolute inset-0 rounded-2xl md:rounded-[48px]"
+                style={{
+                    background: 'linear-gradient(179deg, rgba(0, 193, 157, 0.00) -25.42%, rgba(2, 0, 67, 0.47) 107.11%)',
+                }}
+            ></div>
         </div>
     );
 };
