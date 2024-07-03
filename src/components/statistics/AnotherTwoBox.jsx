3c2f62fe04e803a29e9028b4da616ffd4ca568a0
fojletuhin <fojletuhin@gmail.com>
2024-07-03 21:15:11 +0600

    create Comprehensive Care for Every Patient

--- src/components/statistics/AnotherTwoBox.jsx
+++ src/components/statistics/AnotherTwoBox.jsx
@@ -1,12 +1,14 @@
 
-const AnotherTwoBox = () => {
+const AnotherTwoBox = ({heading, subheading, img}) => {
     return (
         <div className='mt-5'>
             <div className=" border-[0.5px] rounded-3xl bg-[#FFFFF5] border-[#02004333] p-5">
-                <p className="text-[#020043] font-semibold text-5">500+</p>
-                <p className="text-[#343268]">Board-certified <br /> doctors.</p>
+                <p className="text-[#020043] font-semibold text-[40px]">500+</p>
+                <p className="text-[#343268] text-[14px]">Board-certified doctors.</p>
 
-                <img src="https://i.ibb.co/zVNcL0v/contract-1-1.png" alt="" />
+                <div className="flex justify-end">
+                <img className="w-[90px] h-[71px]" src='https://i.ibb.co/zVNcL0v/contract-1-1.png' alt="" />
+                </div>
 
             </div>
         </div>
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,48 +1,98 @@
-import AnotherTwoBox from "./AnotherTwoBox";
 
 
 
 const statistics = () => {
     return (
-        <div className="flex mt-10 gap-5 mb-20">
+        <div className="flex mt-10 gap-5 mb-20 flex-col-reverse md:flex-row">
 
-            <div className="w-[20%]">
-                <div className=" border-[0.5px] rounded-3xl bg-white border-[#02004333] p-5 h-[335px] ">
+            <div className="md:w-[20%]">
+                <div className=" border-[0.5px] rounded-3xl bg-white border-[#02004333] p-5 h-[330px] ">
 
                     <p className="text-[#020043] font-semibold text-[40px]">90%</p>
-                    <p className="opacity-[0.8] mb-6 mt-2 text-[#020043]">Patient satisfaction rate, reflecting our commitment.</p>
+                    <p className="opacity-[0.8] mb-6 mt-2 text-[#020043] text-[14px]">Patient satisfaction rate, reflecting our commitment.</p>
                     <img className="h-[130px] w-[130px] mx-auto items-end" src='https://i.ibb.co/NWjf8ND/Group-12.png' alt="" />
 
                 </div>
 
             </div>
-            <div className="w-[60%]">
-                <div>
-                    <p className="text-center text-5xl font-semibold text-[#020043]">Comprehensive Care <br /> for Every Patient</p>
-                </div>
-                <div className="grid grid-cols-3 gap-5">
-                    <AnotherTwoBox></AnotherTwoBox>
+
+            <div className="md:w-[80%] md:flex gap-5 ">
+                <div className="md:w-[74%]">
+
                     <div>
-                        
+                        <p className="text-center text-2xl font-medium md:text-5xl md:font-semibold text-[#020043]">Comprehensive Care <br /> for Every Patient</p>
                     </div>
-                    <AnotherTwoBox></AnotherTwoBox>
-                    
+                    <div className="md:grid md:grid-cols-3 gap-5 items-end">
+                        <div className='mt-5 mb-5 md:mb-0'>
+                            <div className=" border-[0.5px] rounded-3xl bg-[#FFFFF5] border-[#02004333] p-5">
+                                <p className="text-[#020043] font-semibold text-[40px]">500+</p>
+                                <p className="text-[#343268] text-[14px]">Board-certified doctors.</p>
 
+                                <div className="flex justify-end">
+                                    <img className="w-[90px] h-[71px]" src='https://i.ibb.co/zVNcL0v/contract-1-1.png' alt="" />
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                        <div>
+                            <div className=" border-[0.5px] rounded-3xl bg-[#FBFBFB] border-[#02004333] p-5">
+                                <div className="flex gap-[11px] items-center">
+                                    <p className="text-[#020043] font-semibold text-[40px]">4.8</p>
+                                    <img className="h-8 w-8" src="https://i.ibb.co/HDFW56Q/fi-sr-star.png" alt="" />
+                                </div>
+
+                                <p className="text-[#020043] text-[14px] opacity-[0.8] mb-5">Over 20,000 Patient</p>
+
+
+                                <div className="flex ">
+                                    <img className="w-[35px] h-[35px]" src='https://i.ibb.co/gVSZwDB/Ellipse-2.png' alt="" />
+                                    <img className="w-[35px] h-[35px] -ml-4" src='https://i.ibb.co/YZwnYC4/Ellipse-4.png' alt="" />
+                                    <img className="w-[35px] h-[35px] -ml-4" src='https://i.ibb.co/nfjhCBJ/Ellipse-5.png' alt="" />
+                                    <img className="w-[35px] h-[35px] -ml-4" src='https://i.ibb.co/SyjdvQp/Ellipse-3.png' alt="" />
+                                </div>
+
+                            </div>
+                        </div>
+
+
+
+                        <div className='mt-5 mb-5 md:mb-0'>
+                            <div className=" border-[0.5px] rounded-3xl bg-[#FFFFF5] border-[#02004333] p-5">
+                                <p className="text-[#020043] font-semibold text-[40px]">$5000</p>
+                                <p className="text-[#343268] text-[14px]">Money spend <br />
+                                    for poor patient</p>
+
+                                <div className="flex justify-end">
+                                    <img className="w-[90px] h-[71px]" src='https://i.ibb.co/db7Wmt5/Group-1.png' alt="" />
+                                </div>
+
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+                </div>
+
+
+                <div className="md:w-[26%]">
+                    <div className=" border-[0.5px] rounded-3xl bg-white border-[#02004333] p-5 h-[330px] ">
+
+                        <p className="text-[#020043] font-semibold text-[40px]">50+</p>
+                        <p className="opacity-[0.8] mb-[45px] mt-2 text-[#020043] text-[14px]">Free lession video
+                            for patient.</p>
+                        <img className="h-[130px] w-[130px] mx-auto items-end" src='https://i.ibb.co/hVbgKyx/Group.png' alt="" />
+
+                    </div>
 
                 </div>
 
             </div>
-            <div className="w-[20%]">
-                <div className=" border-[0.5px] rounded-3xl bg-white border-[#02004333] p-5 h-[335px] ">
 
-                    <p className="text-[#020043] font-semibold text-[40px]">50+</p>
-                    <p className="opacity-[0.8] mb-[45px] mt-2 text-[#020043]">Free lession video 
-                    for patient.</p>
-                    <img className="h-[130px] w-[130px] mx-auto items-end" src='https://i.ibb.co/hVbgKyx/Group.png' alt="" />
 
-                </div>
-
-            </div>
         </div>
     );
 };
